b0b14977ebd3f64d8bc5a6d22a6ff80b273db9be
Amir Adel <amir.elbashary@gmail.com>
2019-08-18 16:22:36 +0200

    Implement CRUD for employees

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -1,6 +1,17 @@
 class Admin::EmployeesController < Admin::BaseAdminController
   load_and_authorize_resource
 
+  def new; end
+
+  def create
+    if @employee.save
+      flash[:notice] = "#{@employee.full_name} has joined Fustany Team."
+      redirect_to admin_employees_path
+    else
+      render :new
+    end
+  end
+
   def index; end
 
   def edit; end
@@ -16,13 +27,13 @@ class Admin::EmployeesController < Admin::BaseAdminController
 
   def destroy
     return unless @employee.destroy
-    flash[:notice] = 'Employee was deleted'
+    flash[:notice] = 'Employee was removed from Fustany Team.'
     redirect_to admin_employees_path
   end
 
   private
 
-  def user_params
-    params.require(:employee).permit(:first_name, :last_name)
+  def employee_params
+    params.require(:employee).permit(:email, :password, :password_confirmation, :first_name, :last_name)
   end
 end
