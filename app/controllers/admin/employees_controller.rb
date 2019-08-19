a4dd2c9db8ab11ca5d638eee6961729281e4bbbd
Amir Adel <amir.elbashary@gmail.com>
2019-08-19 15:41:56 +0200

    Complete employee form with image cropper

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -19,7 +19,11 @@ class Admin::EmployeesController < Admin::BaseAdminController
   def update
     if @employee.update(employee_params)
       flash[:notice] = 'Employee has been successfully updated.'
-      redirect_to admin_employees_path
+      if params[:employee][:avatar].present?
+        render :crop ## Render the view for cropping
+      else
+        redirect_to admin_employees_path
+      end
     else
       render :edit
     end
@@ -34,6 +38,11 @@ class Admin::EmployeesController < Admin::BaseAdminController
   private
 
   def employee_params
-    params.require(:employee).permit(:email, :password, :password_confirmation, :first_name, :last_name)
+    params.require(:employee).permit(:email, :password, :password_confirmation, :first_name, :last_name,
+                                     :gender, :birthdate, :address, :social_id, :personal_email, :business_email,
+                                     :mobile_numbers, :landline_numbers, :qualification, :graduation_year,
+                                     :date_of_employment, :job_description, :work_type, :date_of_social_insurance_joining,
+                                     :social_insurance_number, :military_status, :marital_status, :nationality, :vacation_balance,
+                                     :avatar, :avatar_crop_x, :avatar_crop_y, :avatar_crop_w, :avatar_crop_h)
   end
 end
