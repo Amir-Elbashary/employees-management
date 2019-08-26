78ecbdf9076b4cc878145434983172af8f57d9f6
Amir Adel <amir.elbashary@gmail.com>
2019-08-26 14:19:30 +0200

    Deployment configs complete

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '', user: 'deploy', roles: %w{app db web}
+server '157.245.45.90', user: 'deploy', roles: %w{app db web}
 
 
 
