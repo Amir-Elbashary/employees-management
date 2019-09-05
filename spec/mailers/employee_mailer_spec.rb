0554e9e90ef5770120621e76169bf045309a929a
Amir Adel <amir.elbashary@gmail.com>
2019-09-05 12:58:47 +0200

    Remove pending test

--- spec/mailers/employee_mailer_spec.rb
+++ spec/mailers/employee_mailer_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
 RSpec.describe EmployeeMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Tests are enough with mail visual preview
 end
