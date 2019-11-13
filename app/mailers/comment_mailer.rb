b5fa5b82e746539bc1450304aa3df3b94d986095
Amir Adel <amir.elbashary@gmail.com>
2019-11-13 14:40:39 +0200

    Fix comment mail shots

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -2,6 +2,6 @@ class CommentMailer < ApplicationMailer
 
   def new_comment_notifier(comment_id)
     @comment = Comment.find(comment_id)
-    mail(to: @comment.timeline.owner.email, subject: "#{@comment.owner.name} commented on your post")
+    mail(to: @comment.timeline.publisher.email, subject: "#{@comment.commenter.name} commented on your post")
   end
 end
