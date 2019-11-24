3391158d17cef75c7c42491c47c936c545c5a5d0
Amir Adel <amir.elbashary@gmail.com>
2019-11-24 15:56:40 +0200

    Update login screen

--- app/assets/javascripts/admin_login.js
+++
@@ -1,6 +0,0 @@
-//= require admin_login/vendor/jquery/jquery-3.2.1.min.js
-//= require admin_login/vendor/bootstrap/js/popper.js
-//= require admin_login/vendor/bootstrap/js/bootstrap.min.js
-//= require admin_login/vendor/select2/select2.min.js
-//= require admin_login/vendor/tilt/tilt.jquery.min.js
-//= require admin_login/main.js
