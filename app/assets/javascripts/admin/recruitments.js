bc637e34c5a21d586037d1393cb69f1dc2b18c09
Amir Adel <amir.elbashary@gmail.com>
2019-12-04 13:45:40 +0200

    Improve html text editor

--- app/assets/javascripts/admin/recruitments.js
+++ app/assets/javascripts/admin/recruitments.js
@@ -1,6 +1,5 @@
 //= require datatables/jquery.dataTables.min.js
-//= require html5-editor/wysihtml5-0.3.0.js
-//= require html5-editor/bootstrap-wysihtml5.js
+//= require tinymce/tinymce.min.js
 //
 //= require dashboard/data-tables-init.js
 //= require dashboard/select2-init.js
