b1bdb89f7b93859a24658ec85b96ea8d9e5b2a57
Amir Adel <amir.elbashary@gmail.com>
2019-12-08 17:37:04 +0200

    Fix token expiration time

--- app/mailers/attendance_mailer.rb
+++ app/mailers/attendance_mailer.rb
@@ -7,10 +7,10 @@ class AttendanceMailer < ApplicationMailer
          subject: "Attendance summary for #{@attendance.checkin.strftime('%A %d %B %Y')}")
   end
 
-  def checkout_reminder(attendance_id, hmac_secret)
+  def checkout_reminder(attendance_id, hmac_secret, minutes_before_leaving)
     find_attendance(attendance_id)
 
-    generate_token(@attendance, hmac_secret)
+    generate_token(@attendance, hmac_secret, minutes_before_leaving)
 
     mail(to: @attendance.attender.email,
          subject: "Checkout reminder for today #{@attendance.checkin.strftime('%A %d %B %Y')}")
@@ -22,12 +22,12 @@ class AttendanceMailer < ApplicationMailer
     @attendance = Attendance.find(attendance_id)
   end
 
-  def generate_token(attendance, hmac_secret)
+  def generate_token(attendance, hmac_secret, minutes_before_leaving)
     payload = {
       attendance_id: attendance.id,
       attender_type: attendance.attender_type,
       attender_id: attendance.attender_id,
-      exp: (Time.zone.now + 5.minutes).to_i
+      exp: (Time.zone.now + minutes_before_leaving.minutes).to_i
     }
 
     @token = JWT.encode(payload, hmac_secret, 'HS256')
--- app/workers/attendance/checkout_reminder_worker.rb
+++ app/workers/attendance/checkout_reminder_worker.rb
@@ -2,7 +2,7 @@ class Attendance::CheckoutReminderWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'default'
 
-  def perform(attendance_id, hmac_secret)
-    AttendanceMailer.checkout_reminder(attendance_id, hmac_secret).deliver
+  def perform(attendance_id, hmac_secret, minutes_before_leaving)
+    AttendanceMailer.checkout_reminder(attendance_id, hmac_secret, minutes_before_leaving).deliver
   end
 end
